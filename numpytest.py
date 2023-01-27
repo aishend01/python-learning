cead2c015466e236b32d5b569e2d5a5017ebbaf2
aishend01 <109160404+aishend01@users.noreply.github.com>
2023-01-27 19:46:09 +0000

    Add files via upload
    learning arrays with numpy lib

---
+++ numpytest.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr0D = np.array(42)
+arr1D = np.array([42, 75, 83])
+arr2D = np.array([[52, 65 ,76],[45 ,45, 78]])
+arr3D = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print("""Here you have 4 arrays:
+number of dimensions: {}
+{}
+----------
+number of dimensions: {}
+{}
+----------
+number of dimensions: {}
+{}
+----------
+number of dimensions: {}
+{}
+----------
+""".format(arr0D.ndim, arr0D, arr1D.ndim, arr1D, arr2D.ndim, arr2D, arr3D.ndim, arr3D))
+
+
